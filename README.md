54d3f106a0da759109ea5b08f941c6aa4dfcb54c
coolqi <coolqi@gmail.com>
2017-02-28 15:45:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is only a simple portfolio, which uses flexbox for the responsive page.
+
+Instructions: 
+1. Run "npm install" to install dependencies and dev dependencies
+2. Run "gulp" to minify js and css files, there are no js files for this project
+3. open "index.html" in the browser
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	minifyCSS = require('gulp-minify-css'),
+	plumber = require('gulp-plumber');
+
+//minify js files
+gulp.task('scripts', function(){
+	gulp.src('js/*.js')
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest('js/'));
+});
+
+//minify css files
+gulp.task('styles', function() {
+	gulp.src('css/*.css')
+		.pipe(plumber())
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('minCSS/'));
+});
+
+//watch all changes automatically
+gulp.task('watch', function() {
+	gulp.watch('js/*.js',['scripts']);
+	gulp.watch('css/*.css',['styles']);
+})
+
+gulp.task('default', ['scripts', 'styles', 'watch']);
